353201250e7f5adc9c29a3f24c458e4e56db8c5f
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-18 23:08:09 -0600

    Add ItemSerializer with custom attribute :unit_price

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,4 +1,8 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :merchant_id
+
+  attribute :unit_price do |item_object|
+    (item_object.unit_price / 100.0).to_s
+  end
 end
