f10c0bf7fcc6212bb54b40460a9ba91cd2c59110
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-19 05:57:51 -0600

    Add total_revenue_by_merchant (instance method) to Merchant model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -47,4 +47,35 @@ RSpec.describe Merchant, type: :model do
       end
     end
   end
+
+  describe "instance methods" do
+    before(:each) do
+      @customer = create(:customer)
+
+      @merchant_1 = create(:merchant)
+      @item_1 = create(:item, merchant_id: @merchant_1.id)
+      @invoice_1 = create(:invoice, merchant_id: @merchant_1.id, customer_id: @customer.id, created_at: "2012-03-27T14:54:02.000Z", updated_at: "2012-03-27T14:54:02.000Z")
+      @invoice_item_1 = create(:invoice_item, invoice_id: @invoice_1.id, item_id: @item_1.id, unit_price: 1, quantity: 2)
+      @transaction_1 = create(:transaction, invoice_id: @invoice_1.id, result: "success")
+
+      @merchant_2 = create(:merchant)
+      @item_2 = create(:item, merchant_id: @merchant_2.id)
+      @invoice_2 = create(:invoice, merchant_id: @merchant_2.id, customer_id: @customer.id, created_at: "2012-03-27T14:54:02.000Z", updated_at: "2012-03-27T14:54:02.000Z")
+      @invoice_item_2 = create(:invoice_item, invoice_id: @invoice_2.id, item_id: @item_2.id, unit_price: 3, quantity: 4)
+      @invoice_item_4 = create(:invoice_item, invoice_id: @invoice_2.id, item_id: @item_1.id, unit_price: 1, quantity: 5)
+      @transaction_2 = create(:transaction, invoice_id: @invoice_2.id, result: "failed")
+
+      @merchant_3 = create(:merchant)
+      @item_3 = create(:item, merchant_id: @merchant_3.id)
+      @invoice_3 = create(:invoice, merchant_id: @merchant_3.id, customer_id: @customer.id, created_at: "2019-03-27T14:54:02.000Z", updated_at: "2019-03-27T14:54:02.000Z")
+      @invoice_item_3 = create(:invoice_item, invoice_id: @invoice_3.id, item_id: @item_3.id, unit_price: 5, quantity: 6)
+      @transaction_3 = create(:transaction, invoice_id: @invoice_3.id, result: "success")
+    end
+
+    describe "#total_revenue_by_merchant" do
+      it "returns the total revenue for that merchant across successful transactions" do
+        expect((@merchant_1.total_revenue_by_merchant) / 100.0).to eq(0.02)
+      end
+    end
+  end
 end
