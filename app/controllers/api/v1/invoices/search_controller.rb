438afae2272791184760c9708306c634cd22cd3a
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-19 01:49:16 -0600

    Add methods (index, show, invoice_params) to Api::V1::Invoices::SearchController

---
+++ app/controllers/api/v1/invoices/search_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::Invoices::SearchController < ApplicationController
+  def index
+    invoice =
+    render json: InvoiceSerializer.new(Invoice.where(invoice_params).order(:id))
+  end
+
+  def show
+    render json: InvoiceSerializer.new(Invoice.find_by(invoice_params))
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:id, :customer_id, :merchant_id, :status, :created_at, :updated_at)
+  end
+end
