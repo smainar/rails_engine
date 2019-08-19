057bba807e9c597525303b841c87481e2f8dd4eb
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-19 05:56:21 -0600

    Add route for Merchant Business Intel Endpoint - returns the total revenue for that merchant across successful transactions

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
         get "/:id/invoices", to: "invoices#index"
         get "/most_revenue", to: "most_revenue#index"
         get "/most_items", to: "most_items#index"
+        get "/:id/revenue", to: "revenue#show"
       end
 
       namespace :items do
