336c72438bfec9c394daa99ae22cfd0e9bee9bcf
Stella Mainar <mainars09@alumni.hanover.edu>
2019-08-18 23:07:08 -0600

    Add routes for Item Record Endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,14 @@ Rails.application.routes.draw do
         get "/most_items", to: "most_items#index"
       end
 
+      namespace :items do
+        get "/find", to: "search#show"
+        get "/random", to: "search#show"
+        get "/find_all", to: "search#index"
+      end
+
       resources :merchants, only: [:index, :show]
+      resources :items, only: [:index, :show]
     end
   end
 end
